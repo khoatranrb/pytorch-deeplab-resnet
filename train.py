184c483b19d658a48af2ad25ea43bcebb3fe04be
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2022-03-16 00:28:57 +0700

    Update train.py

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def read_file(path_to_file):
     return img_list
 
 def chunker(seq, size):
- return (seq[pos:pos+size] for pos in xrange(0,len(seq), size))
+ return (seq[pos:pos+size] for pos in range(0,len(seq), size))
 
 def resize_label_batch(label, size):
     label_resized = np.zeros((size,size,1,label.shape[3]))
