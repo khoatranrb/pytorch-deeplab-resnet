9033f0d5e476ba4d5bcd23caec569e35610eab7c
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2022-03-16 00:26:13 +0700

    Update deeplab_resnet.py

--- deeplab_resnet.py
+++ deeplab_resnet.py
@@ -105,19 +105,19 @@ class Classifier_Module(nn.Module):
 
     def __init__(self,dilation_series,padding_series,NoLabels):
         super(Classifier_Module, self).__init__()
-    self.conv2d_list = nn.ModuleList()
-    for dilation,padding in zip(dilation_series,padding_series):
-        self.conv2d_list.append(nn.Conv2d(2048,NoLabels,kernel_size=3,stride=1, padding =padding, dilation = dilation,bias = True))
+        self.conv2d_list = nn.ModuleList()
+        for dilation,padding in zip(dilation_series,padding_series):
+            self.conv2d_list.append(nn.Conv2d(2048,NoLabels,kernel_size=3,stride=1, padding =padding, dilation = dilation,bias = True))
 
-        for m in self.conv2d_list:
-            m.weight.data.normal_(0, 0.01)
+            for m in self.conv2d_list:
+                m.weight.data.normal_(0, 0.01)
 
 
     def forward(self, x):
-    out = self.conv2d_list[0](x)
-    for i in range(len(self.conv2d_list)-1):
-        out += self.conv2d_list[i+1](x)
-        return out
+        out = self.conv2d_list[0](x)
+        for i in range(len(self.conv2d_list)-1):
+            out += self.conv2d_list[i+1](x)
+            return out
 
 
 
@@ -166,7 +166,7 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
     def _make_pred_layer(self,block, dilation_series, padding_series,NoLabels):
-    return block(dilation_series,padding_series,NoLabels)
+        return block(dilation_series,padding_series,NoLabels)
 
     def forward(self, x):
         x = self.conv1(x)
